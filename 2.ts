32eb73d8060621b1a7b5766d79972c780d6f2692
Gnlow <gnlow@pm.me>
2025-08-24 08:22:30 +0900

    Create 2.ts

---
+++ 2.ts
@@ -0,0 +1,47 @@
+class Animal {
+    name
+    get age() {
+        console.log("Who get my age?")
+        return 123
+    }
+    constructor(name: string) {
+        console.log("I'm being constructed!")
+        this.name = name
+    }
+}
+
+class Dog extends Animal {
+    bark() {
+        console.log(`Bark! i'm ${this.name}.`)
+    }
+}
+
+class Cat extends Animal {
+    meow() {
+        console.log(`Meow! i'm ${this.name}.`)
+    }
+}
+
+// deno-lint-ignore no-explicit-any
+type Args<Class extends { new(...args: any): any }> =
+Class extends { new(...args: infer Args): infer _ }
+    ? Args
+    : never
+
+interface Chimera extends Dog, Cat {}
+
+const Chimera = function (this: Chimera, ...args: Args<typeof Animal>) {
+    Object.assign(this, new Animal(...args))
+    ;[Dog, Cat, Animal].forEach(Class => {
+        const desc = Object.getOwnPropertyDescriptors(Class.prototype)
+
+        Object.defineProperties(this, desc)
+    })
+} as unknown as { new(...args: Args<typeof Animal>): Chimera }
+
+const chimera = new Chimera("John")
+
+console.log(chimera)
+console.log(chimera.age)
+chimera.bark()
+chimera.meow()

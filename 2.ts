51dad3778f2241c2b2aafd2f821295131179ad6e
Gnlow <gnlow@pm.me>
2025-08-24 13:10:39 +0900

    Update 2.ts

--- 2.ts
+++ 2.ts
@@ -11,12 +11,20 @@ class Animal {
 }
 
 class Dog extends Animal {
+    constructor(name: string) {
+        super(name)
+        console.log(`Dog ${name} is being counstructed!`)
+    }
     bark() {
         console.log(`Bark! i'm ${this.name}.`)
     }
 }
 
 class Cat extends Animal {
+    constructor(name: string) {
+        super(name)
+        console.log(`Cat ${name} is being counstructed!`)
+    }
     meow() {
         console.log(`Meow! i'm ${this.name}.`)
     }
@@ -31,11 +39,11 @@ Class extends { new(...args: infer Args): infer _ }
 interface Chimera extends Dog, Cat {}
 
 const Chimera = function (this: Chimera, ...args: Args<typeof Animal>) {
-    Object.assign(this, new Animal(...args))
     ;[Dog, Cat, Animal].forEach(Class => {
         const desc = Object.getOwnPropertyDescriptors(Class.prototype)
 
         Object.defineProperties(this, desc)
+        Object.assign(this, new Class(...args))
     })
 } as unknown as { new(...args: Args<typeof Animal>): Chimera }
 
